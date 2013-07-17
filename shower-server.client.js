6935a59d8346868daa251c4663ba0900c0fb865f
Igor Shevchenko <cyberskunk@gmail.com>
2013-07-17 16:47:20 +0400

    basic buggy implementation

--- shower-server.client.js
+++ shower-server.client.js
@@ -2,18 +2,36 @@
 (function (){
     var socket = io.connect('http://' + location.hostname + location.pathname.replace(/(^\/.*)\/$/, '$1'));
     var master = location.search.replace(/^.*master=([^&]*)$/, '$1');
+    var connected = true;
 
     socket.on('connect', function () {
-        socket.emit('setMaster', master, function (data) {
-            master = data;
-            if (master) {
-                // only master emits hashchanges
-                addEvent(window, 'hashchange', browserHashchangeHandler);
-            } else {
-                // master doesn't subscribe to hashchanges
-                socket.on('hashchange', socketHashchangeHandler);
-            }
-        });
+        socket.emit('setMaster', master, setMasterCallback);
+    });
+
+
+    var button = document.createElement('div');
+
+    button.className = 'changeUpdateStatus';
+    button.innerHTML = 'Disconnect';
+    button.style.position = 'fixed';
+    button.style.left = '50px';
+    button.style.bottom = '50px';
+    button.style.zIndex = 100000;
+    button.style.padding = '0.2em';
+    button.style.border = '1px solid black';
+    button.style.backgroundColor = 'white';
+    document.body.appendChild(button);
+
+    addEvent(button, 'click', function () {
+        if (connected) {
+            button.innerHTML = 'Connect';
+            socket.removeListener('hashchange', socketHashchangeHandler);
+        } else {
+            button.innerHTML = 'Disonnect';
+            socket.emit('setMaster', master, setMasterCallback);
+            socket.on('hashchange', socketHashchangeHandler);
+        }
+        connected = !connected;
     });
 
     function addEvent(elem, event, fn) {
@@ -33,5 +51,16 @@
 
     function browserHashchangeHandler () {
         socket.emit('hashchange', {hash: location.hash});
+    }
+
+    function setMasterCallback (data) {
+        master = data;
+        if (master) {
+            // only master emits hashchanges
+            addEvent(window, 'hashchange', browserHashchangeHandler);
+        } else {
+            // master doesn't subscribe to hashchanges
+            socket.on('hashchange', socketHashchangeHandler);
+        }
     }
 })();
