d931d2acc6faa55a32eb4d91dd83134c06e2e91d
Igor Shevchenko <cyberskunk@gmail.com>
2014-02-12 23:29:25 +0400

    refactoring, queryParams and debug added at client side

--- shower-server.client.js
+++ shower-server.client.js
@@ -1,37 +1,27 @@
 /*global io:false */
 (function (){
-    var socket = io.connect('http://' + location.hostname + location.pathname.replace(/(^\/.*)\/$/, '$1'));
-    var master = location.search.replace(/^.*master=([^&]*)$/, '$1');
-    var connected = true;
+    var socket = io.connect('http://' + location.hostname + location.pathname.replace(/(^\/.*)\/$/, '$1')),
+        queryParams = (function () {
+            return location.search.substring(1).split('&').map(function (pair) {
+                return pair.split('=');
+            }).reduce(function (prev, current) {
+                prev[current[0]] = current.length === 2 ? current[1] : true;
+                return prev;
+            }, {});
+        })(),
+        master = false,
+        debug = function (message) {
+            if (queryParams.debug) {
+                console.log(message);
+            }
+        },
+        connected = false;
 
     socket.on('connect', function () {
-        socket.emit('setMaster', master, setMasterCallback);
-    });
-
-
-    var button = document.createElement('div');
-
-    button.className = 'changeUpdateStatus';
-    button.innerHTML = 'Disconnect';
-    button.style.position = 'fixed';
-    button.style.left = '50px';
-    button.style.bottom = '50px';
-    button.style.zIndex = 100000;
-    button.style.padding = '0.2em';
-    button.style.border = '1px solid black';
-    button.style.backgroundColor = 'white';
-    document.body.appendChild(button);
-
-    addEvent(button, 'click', function () {
-        if (connected) {
-            button.innerHTML = 'Connect';
-            socket.removeListener('hashchange', socketHashchangeHandler);
-        } else {
-            button.innerHTML = 'Disonnect';
-            socket.emit('setMaster', master, setMasterCallback);
-            socket.on('hashchange', socketHashchangeHandler);
-        }
-        connected = !connected;
+        connected = true;
+        debug('connected, master=' + queryParams.master);
+        socket.emit('setMaster', queryParams.master, setMasterCallback);
+        addDisconnectButton()
     });
 
     function addEvent(elem, event, fn) {
@@ -46,21 +36,67 @@
     }
 
     function socketHashchangeHandler(data) {
-        location.hash = data;
+        if (connected) {
+            debug('RECEIVED: ' + JSON.stringify(data));
+            location.hash = data;
+        }
     }
 
     function browserHashchangeHandler() {
-        socket.emit('hashchange', { hash: location.hash });
+        var dataToEmit = { hash: location.hash };
+        if (connected) {
+            debug('EMIT: ' + JSON.stringify(dataToEmit))
+            socket.emit('hashchange', dataToEmit);
+        }
     }
 
-    function setMasterCallback(data) {
-        master = data;
+    function setMasterCallback(isClientMaster) {
+        master = isClientMaster;
         if (master) {
+            debug('OK, adding event handler on hashchange');
             // only master emits hashchanges
             addEvent(window, 'hashchange', browserHashchangeHandler);
+            // emit current position
+            browserHashchangeHandler();
         } else {
+            debug('Nyan, subscribing to socket messages');
             // master doesn't subscribe to hashchanges
             socket.on('hashchange', socketHashchangeHandler);
         }
     }
+
+    function addDisconnectButton() {
+        var button = document.createElement('div');
+
+        button.className = 'changeUpdateStatus';
+        button.innerHTML = 'Disconnect';
+        button.style.position = 'fixed';
+        button.style.left = '50px';
+        button.style.top = '50px';
+        button.style.zIndex = 100000;
+        button.style.padding = '0.2em';
+        button.style.border = '1px solid black';
+        button.style.backgroundColor = 'white';
+        document.body.appendChild(button);
+
+        addEvent(button, 'click', function () {
+            connected = !connected;
+
+            if ( ! connected) {
+                button.innerHTML = 'Connect';
+                if ( ! master) {
+                    socket.removeAllListeners();
+                }
+            } else {
+                button.innerHTML = 'Disconnect';
+                if (master) {
+                    // master sends update on re-connection
+                    browserHashchangeHandler();
+                } else {
+                    socket.emit('setMaster', queryParams.master, setMasterCallback);
+                }
+            }
+        });
+    }
+
 })();
